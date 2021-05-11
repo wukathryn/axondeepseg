0a24c3df526f43e8eaa7f8bbac8ff767dd3338de
Marie-Helene Bourget <bourgetmarieh@gmail.com>
2021-05-11 10:37:32 -0400

    ads_utils.py: Use tifffile, imagescodecs and cv2 to read large tif files

--- AxonDeepSeg/ads_utils.py
+++ AxonDeepSeg/ads_utils.py
@@ -17,6 +17,8 @@ from tqdm import tqdm
 import raven
 import imageio
 import numpy as np
+import tifffile
+import cv2
 
 DEFAULT_CONFIGFILE = "axondeepseg.cfg"
 
@@ -255,15 +257,17 @@ def imread(filename, bitdepth=8):
     """ Read image and convert it to desired bitdepth without truncation.
     """
     if 'tif' in str(filename):
-        raw_img = imageio.imread(filename, format='tiff-pil')
+        raw_img = tifffile.imread(filename)
         if len(raw_img.shape) > 2:
-            raw_img = imageio.imread(filename, format='tiff-pil', as_gray=True)
+            raw_img = tifffile.imread(filename)
+            raw_img = cv2.cvtColor(raw_img, cv2.COLOR_RGB2GRAY)
     else:
         raw_img = imageio.imread(filename)
         if len(raw_img.shape) > 2:
             raw_img = imageio.imread(filename, as_gray=True)
 
     img = imageio.core.image_as_uint(raw_img, bitdepth=bitdepth)
+
     return img
 
 def imwrite(filename, img, format='png'):
