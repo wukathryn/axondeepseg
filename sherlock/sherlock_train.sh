e7b9ac75b2206cd1a962cc94f4d90f901df80bd1
wukathryn <wukathryn@gmail.com>
2022-01-25 21:52:37 -0800

    fixed error in sbatch file

--- sherlock/sherlock_train.sh
+++ sherlock/sherlock_train.sh
@@ -4,16 +4,17 @@
 #SBATCH -t 1-0
 
 model_dir=''
-trainingset=''
-while getopts 'm:t' flag; do
+trainingset_path=''
+
+while getopts 'm:t:' flag; do
     case "${flag}" in
         m) model_dir=${OPTARG};;
         t) trainingset_path=${OPTARG};;
     esac
 done
 
-echo "model directory: " + $model_dir
-echo "training set: " + $trainingset_path
+echo "model directory: " $model_dir
+echo "training set: " $trainingset_path
 echo 'This script is running on:'
 hostname
 
