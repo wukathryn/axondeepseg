d4fd861618f10ab9434ce50940e0c85df01b930a
wukathryn <wukathryn@gmail.com>
2022-01-25 21:37:47 -0800

    minor edit

--- sherlock/sherlock_train.sh
+++ sherlock/sherlock_train.sh
@@ -8,17 +8,17 @@ trainingset=''
 while getopts 'm:t' flag; do
     case "${flag}" in
         m) model_dir=${OPTARG};;
-        t) trainingset=${OPTARG};;
+        t) trainingset_path=${OPTARG};;
     esac
 done
 
 echo "model directory: " + $model_dir
-echo "training set: " + $trainingset
+echo "training set: " + $trainingset_path
 echo 'This script is running on:'
 hostname
 
 
 source ~/.bashrc
 conda activate ads_venv
-python ../AxonDeepSeg/train_from_config.py -m $model_dir -t $trainingset
+python ../AxonDeepSeg/train_from_config.py -m $model_dir -t $trainingset_path
 
